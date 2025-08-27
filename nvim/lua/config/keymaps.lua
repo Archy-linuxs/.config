1cb65d2cd50ba44d0f4ace0744cc158cc3a70f58
Archy <archy_linuxs@internet.ru>
2025-08-27 16:02:06 +0300

    new settings

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+vim.keymap.set("i","<C-j>", "<ESC>:w<CR>")
+vim.keymap.set("n","<C-j>", "<ESC>:w<CR>")
+vim.keymap.set("n", "<C-k>", ":q<CR>")
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory in Oil"})
+
+
+
