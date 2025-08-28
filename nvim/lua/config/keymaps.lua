9f69171ab89fa39dfc3061bc1886df3fbab329f1
Archy <archy_linuxs@internet.ru>
2025-08-28 13:36:59 +0300

    vim config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@ vim.keymap.set("i","<C-j>", "<ESC>:w<CR>")
 vim.keymap.set("n","<C-j>", "<ESC>:w<CR>")
 vim.keymap.set("n", "<C-k>", ":q<CR>")
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory in Oil"})
+vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end,
+  {desc="Open Diagnostics in Float"})
 
 
 
